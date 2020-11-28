613209750259630be918b9d0ad082f47bf22c2bc
Martijn Casteel <martijncasteel@users.noreply.github.com>
2020-11-28 22:35:14 +0100

    Initial commit with the simulator on a seperate branch

---
+++ frame-simulator/__main__.py
@@ -0,0 +1,44 @@
+"""
+Simple simulator displaying received image on a 16 by 16 pygame 
+window. Listening to port and display on new receive.
+"""
+import os
+import signal
+import argparse
+
+from .controller import Controller
+
+def handler(signum, _):
+    """ signal handler """
+
+    if(signum == signal.SIGUSR1):
+        if controller:
+            controller.reload()
+        return
+
+    print(f' interupt has been caught ({signum}), shutting down...')
+
+    if controller:
+        controller.shutdown()
+
+    exit(0)
+
+if __name__ == '__main__':
+
+    signal.signal(signal.SIGINT, handler) 
+    signal.signal(signal.SIGUSR1, handler)
+
+
+    parser = argparse.ArgumentParser(description='Frame simulator')
+    parser.add_argument('-directory', '-d', help='image directory for binaries', default='/home/martijn/Pictures/frame-images')
+    args = parser.parse_args()
+
+    controller = Controller(args.directory)
+    while controller.running:
+        # loop through all files
+        controller.run()
+
+        # re-populate the files list
+        controller.reload()
+
+    exit(0)
