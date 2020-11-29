d5ab0a2da0e5a85a655118859affd79fc1cad5d7
Martijn Casteel <martijncasteel@users.noreply.github.com>
2020-11-29 13:47:39 +0100

    Frame application running on a raspberry pi zero

---
+++ README.md
@@ -0,0 +1,37 @@
+# frame simulator
+Frame is a small led-matrix controller build for raspberry pi. This simulator uses Pygame to simulate without an actual display. Frame uses a custom file format, with the colors for every pixel. I used a small converter script to create `.frame` files from gifs.  I wouldn't have gotten far without [pygif](https://github.com/robert-ancell/pygif) of Robert Ancell and the explanation of [Christophe Tronche](https://tronche.com/computer-graphics/gif/gif89a.html#image-descriptor).
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. A step by step series of examples that tell you how to get a development env running
+
+```bash
+# setting up environment
+python3 -m venv env
+source env/bin/activate
+
+# install pygame once
+pip install pygame
+
+# run the program
+python -m frame-simulator /directory/to/frame-files
+
+# reload available files from image folder
+ps aux | grep -i "python -m frame-simulator"
+kill -USR1 $pid
+```
+
+## frame.service
+```bash
+
+
+```
+
+## Contributing
+
+Please read [the contributing guidelines](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us. We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags](https://github.com/martijncasteel/frame/tags) on this repository. 
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
---
+++ frame/controller.py
@@ -0,0 +1,94 @@
+"""
+This controller is used to render a 16 by 16 image on screen using
+pygame. Reading it contents of a custom binary file.
+"""
+import os
+import time
+
+import board
+import neopixel
+
+from pathlib import Path
+from .images import Image, ImageException
+
+
+class Controller():
+    """
+    Pygame is used as a simulator to test the rendering
+    """
+
+    def __init__(self, directory, brightness=5):
+        self.running = True
+        self._reload = False
+
+        self.directory = Path(directory)
+        
+        self._files = self.directory.glob('*.frame')
+        self._array = neopixel.NeoPixel(board.D18, 256, brightness=brightness/255, auto_write=False)
+        self._delay_until = time.time()
+
+
+    def __getitem__(self, pos):
+        return self._array[self.__alternate(*pos)]
+
+    def __setitem__(self, pos, value):
+        self._array[self.__alternate(*pos)] = value
+
+    
+    def run(self):
+        self._reload = False
+
+        for file in self._files:
+            try:
+                with file.open(mode='rb') as f:
+
+                    image = Image(f)
+                    image.display(self)
+
+            except ImageException as exception:
+                print(exception)
+                continue
+
+            except FileNotFoundError as exception:
+                print(f'file not found: {exception.filename}')
+                continue
+
+            if self._reload:
+                return
+
+
+    def draw(self):
+        self._array.show()
+        
+
+    def sleep(self, next_showing):
+        seconds = self._delay_until - time.time()
+        
+        if seconds > 0:
+            time.sleep(seconds)
+
+        self._delay_until = next_showing + time.time()
+
+
+    def reload(self, verbose=False):
+        if verbose:
+            print('reloading files..')
+        
+        self._files = self.directory.glob('*.frame')
+        self._reload = True
+
+    def shutdown(self):
+        self._array.fill((0, 0, 0))
+        self._array.show()
+
+
+    @staticmethod
+    def __alternate(x, y):
+        index = y * 16
+
+        if y % 2 == 0:
+            index += 15 - x
+        else:
+            index += x
+        
+        return index
